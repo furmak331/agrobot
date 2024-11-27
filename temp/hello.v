1effb6fb2862c8c55ba9efd284924621498a7bb7
Furqan Makhdoomi <145181976+furmak331@users.noreply.github.com>
2024-11-27 09:42:20 +0530

    Delete temp directory

--- temp/hello.v
+++
@@ -1,8 +0,0 @@
-module tb;
-
-  // Initial block is another construct typically used to initialize signal nets and variables for simulation
-	initial
-		// Verilog supports displaying signal values to the screen so that designers can debug whats wrong with their circuit
-		// For our purposes, we'll simply display "Hello World"
-		$display ("Hello World !");
-endmodule
